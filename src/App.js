235b538955e760c80eac1771ad65ec7266df63d9
Alvin <wangalvin23@gmail.com>
2023-05-19 21:11:05 -0700

    Add Pokemon Navbar and Display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function ClickerBut() {
+  console.log("rendered");
+  const [value, setValue] = useState(0);
+  function clicked() {
+    console.log(value);
+    setValue(value + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="clicker-button" onClick={clicked}>
+      {value}
+    </button>
   );
 }
+const Entry = ({ selected, pokemon, handleClick }) => {
+  return (
+    <>
+      <button className="entry" onClick={() => handleClick(pokemon.id)}>
+        {`${selected === pokemon.id ? "X - " : ""}${pokemon.name}`}
+      </button>
+    </>
+  );
+};
+
+const POKEMON = [
+  { id: 133, name: "Eevee", img: "/pokemonImg/eevee.png" },
+  { id: 134, name: "Vaporeon", img: "/pokemonImg/vaporeon.png" },
+  { id: 135, name: "Jolteon", img: "/pokemonImg/jolteon.png" },
+  { id: 136, name: "Flareon", img: "/pokemonImg/flareon.png" },
+  { id: 196, name: "Espeon", img: "/pokemonImg/espeon.png" },
+  { id: 197, name: "Umbreon", img: "/pokemonImg/umbreon.png" },
+  { id: 470, name: "Leafeon", img: "/pokemonImg/leafeon.png" },
+  { id: 471, name: "Glaceon", img: "/pokemonImg/glaceon.png" },
+  { id: 700, name: "Sylveon", img: "/pokemonImg/sylveon.png" },
+];
+
+const EntryArray = ({ selected, setSelected }) => {
+  const handleClick = (id) => {
+    setSelected(id);
+  };
+
+  return (
+    <>
+      {POKEMON.map((pokemon) => {
+        return (
+          <Entry
+            key={pokemon.id}
+            selected={selected}
+            pokemon={pokemon}
+            handleClick={handleClick}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const PokemonDisplay = ({ displayPokemon }) => {
+  return <>{displayPokemon && <img src={displayPokemon.img} />}</>;
+};
+
+const App = () => {
+  const [selected, setSelected] = useState(null);
+  const displayPokemon =
+    selected !== null
+      ? POKEMON.find((pokemon) => pokemon.id === selected)
+      : null;
+
+  return (
+    <>
+      <div className="navbar">
+        <EntryArray selected={selected} setSelected={setSelected} />
+      </div>
+      <div className="display">
+        <PokemonDisplay displayPokemon={displayPokemon} />
+      </div>
+    </>
+  );
+};
 
 export default App;
